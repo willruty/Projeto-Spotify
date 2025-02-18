f9383129ba05166879df03a23d6d1a4e587777bd
willruty <williamrutynamacedo@gmail.com>
2025-02-18 19:49:55 -0300

    Adjustments of api and host

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,9 @@
 import axios from "axios";
 import 'dotenv/config';
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+const URL = "https://projeto-spotify-0k9g.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
