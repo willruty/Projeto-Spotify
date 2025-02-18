c2cd2d9c4427cd1e8980ed19d89a9f52c70ef38c
willruty <williamrutynamacedo@gmail.com>
2025-02-18 19:33:42 -0300

    Adjustments for deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,9 @@
 // Fetch ou Axios
 import axios from "axios";
+import 'dotenv/config';
 
-const URL = "http://localhost:3000";
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
